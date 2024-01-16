482b01dfcf08381628ca8f4e5529af54bae6cc63
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-16 14:04:59 +0530

    Implement Material theme

--- composeApp/src/commonMain/kotlin/presentation/App.kt
+++ composeApp/src/commonMain/kotlin/presentation/App.kt
@@ -1,16 +1,16 @@
 package presentation
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import org.koin.compose.KoinContext
 import org.koin.compose.koinInject
 import presentation.home.HomeScreen
 import presentation.home.HomeViewModel
+import presentation.theme.EncoreTheme
 
 @Composable
 fun App() {
     KoinContext {
-        MaterialTheme {
+        EncoreTheme {
             val homeViewModel: HomeViewModel = koinInject()
 
             HomeScreen(
