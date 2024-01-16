a55b930ca3e7953f3e259657c7147d3e6207751a
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-16 14:04:13 +0530

    Implement Material theme

---
+++ composeApp/src/iosMain/kotlin/presentation/theme/Theme.ios.kt
@@ -0,0 +1,24 @@
+package presentation.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun EncoreTheme(
+    darkTheme: Boolean,
+    dynamicColor: Boolean,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (!darkTheme) {
+        lightScheme
+    } else {
+        darkScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        shapes = Shapes,
+        typography = Typography,
+        content = content
+    )
+}
