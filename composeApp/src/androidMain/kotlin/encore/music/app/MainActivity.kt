6ed24c367d41c50a05e281ba9051ad1ec1e9770c
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-18 12:27:42 +0530

    Installed splash screen and enabled edge to edge

--- composeApp/src/androidMain/kotlin/encore/music/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/encore/music/app/MainActivity.kt
@@ -5,8 +5,10 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.arkivanov.decompose.defaultComponentContext
 import navigation.DefaultRootComponent
 import navigation.RootComponent
@@ -17,6 +19,8 @@ class MainActivity : ComponentActivity() {
     private lateinit var rootComponent: RootComponent
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         val authorizationCode = intent.data?.extractAuthorizationCode()
