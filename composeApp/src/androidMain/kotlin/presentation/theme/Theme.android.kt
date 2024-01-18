7a343cd8daffc580a8ccddd6aa5ea960da03705a
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-18 12:26:42 +0530

    Removed system ui configurations

--- composeApp/src/androidMain/kotlin/presentation/theme/Theme.android.kt
+++ composeApp/src/androidMain/kotlin/presentation/theme/Theme.android.kt
@@ -1,16 +1,11 @@
 package presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 @Composable
 actual fun EncoreTheme(
@@ -26,14 +21,6 @@ actual fun EncoreTheme(
 
         darkTheme -> darkScheme
         else -> lightScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
     }
 
     MaterialTheme(
