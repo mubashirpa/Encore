da9971f72840ead58a5f09ec566371a02a442b68
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-20 12:40:56 +0530

    Injected MainViewModel

--- composeApp/src/commonMain/kotlin/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/di/ViewModelModule.kt
@@ -1,16 +1,23 @@
 package di
 
 import org.koin.dsl.module
+import presentation.MainViewModel
 import presentation.home.HomeViewModel
 
 val viewModelModule = module {
     factory {
         HomeViewModel(
-            requestAuthAccessTokenUseCase = get(),
             getAccessTokenUseCase = get(),
             getFeaturedPlaylistsUseCase = get(),
-            getUsersTopTracksUseCase = get(),
+            getUsersTopTracksUseCase = get()
+        )
+    }
+    factory {
+        MainViewModel(
             requestUserAuthorizationUseCase = get(),
+            requestAuthAccessTokenUseCase = get(),
+            getAccessTokenUseCase = get(),
+            refreshTokenUseCase = get(),
             urlLauncher = get()
         )
     }
