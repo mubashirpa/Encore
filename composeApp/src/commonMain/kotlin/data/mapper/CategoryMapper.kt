e162f55fe8e5c34de1ad3ef530087215b2618c0c
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-21 20:49:44 +0530

    Removed

--- composeApp/src/commonMain/kotlin/data/mapper/CategoryMapper.kt
+++
@@ -1,8 +0,0 @@
-package data.mapper
-
-import data.remote.dto.category.CategoryDto
-import domain.model.categories.Category
-
-fun CategoryDto.toCategory(): Category {
-    return Category(categories)
-}
--- composeApp/src/commonMain/kotlin/domain/model/categories/Category.kt
+++
@@ -1,7 +0,0 @@
-package domain.model.categories
-
-import data.remote.dto.category.Categories
-
-data class Category(
-    val categories: Categories? = null
-)
