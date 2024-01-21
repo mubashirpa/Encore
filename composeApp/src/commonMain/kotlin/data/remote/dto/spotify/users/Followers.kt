14e6ead6666fb7c5f5762ec24b6675f9475805ab
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-21 20:44:26 +0530

    Optimised data classes. Reformat code.

--- composeApp/src/commonMain/kotlin/data/remote/dto/spotify/users/Followers.kt
+++ composeApp/src/commonMain/kotlin/data/remote/dto/spotify/users/Followers.kt
@@ -1,9 +1,9 @@
-package data.remote.dto.users
+package data.remote.dto.spotify.users
 
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Followers(
     val href: String? = null,
-    val total: Int? = 0
+    val total: Int? = null
 )
