0720dd3b8a51c8e030cb87438fe5b8b10a8f054d
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-24 15:29:27 +0530

    Added Decompose util functions

--- composeApp/src/commonMain/kotlin/navigation/utils/DecomopseUtils.kt
+++ composeApp/src/commonMain/kotlin/navigation/utils/DecomopseUtils.kt
@@ -1,11 +1,18 @@
 package navigation.utils
 
+import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.value.Value
+import com.arkivanov.essenty.lifecycle.Lifecycle
 import com.arkivanov.essenty.lifecycle.LifecycleOwner
 import com.arkivanov.essenty.lifecycle.doOnDestroy
+import com.arkivanov.essenty.lifecycle.subscribe
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 fun LifecycleOwner.coroutineScope(
@@ -14,4 +21,37 @@ fun LifecycleOwner.coroutineScope(
     val scope = CoroutineScope(context + SupervisorJob())
     lifecycle.doOnDestroy(scope::cancel)
     return scope
+}
+
+fun <T : Any> StateFlow<T>.asValue(
+    lifecycle: Lifecycle,
+    context: CoroutineContext = Dispatchers.Main.immediate,
+): Value<T> =
+    asValue(
+        initialValue = value,
+        lifecycle = lifecycle,
+        context = context,
+    )
+
+fun <T : Any> Flow<T>.asValue(
+    initialValue: T,
+    lifecycle: Lifecycle,
+    context: CoroutineContext = Dispatchers.Main.immediate,
+): Value<T> {
+    val value = MutableValue(initialValue)
+    var scope: CoroutineScope? = null
+    lifecycle.subscribe(
+        onStart = {
+            scope = CoroutineScope(context).apply {
+                launch {
+                    collect { value.value = it }
+                }
+            }
+        },
+        onStop = {
+            scope?.cancel()
+            scope = null
+        }
+    )
+    return value
 }
