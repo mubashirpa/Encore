b3dc9698c5221b6704779604f7e6bccbc3c64481
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-27 23:12:05 +0530

    Renamed some classes with proper names

--- composeApp/src/commonMain/kotlin/domain/usecase/spotify/users/GetUsersTopTracksUseCase.kt
+++ composeApp/src/commonMain/kotlin/domain/usecase/spotify/users/GetUsersTopTracksUseCase.kt
@@ -4,14 +4,14 @@ import core.Result
 import data.mapper.toUsersTrackItem
 import domain.model.spotify.users.top_items.UsersTrackItem
 import domain.repository.SpotifyRepository
-import domain.repository.TimeRange
+import domain.repository.UsersTopItemTimeRange
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class GetUsersTopTracksUseCase(private val spotifyRepository: SpotifyRepository) {
     operator fun invoke(
         accessToken: String,
-        timeRange: TimeRange = TimeRange.MEDIUM_TERM,
+        timeRange: UsersTopItemTimeRange = UsersTopItemTimeRange.MEDIUM_TERM,
         limit: Int = 20,
         offset: Int = 0,
     ): Flow<Result<List<UsersTrackItem>>> =
