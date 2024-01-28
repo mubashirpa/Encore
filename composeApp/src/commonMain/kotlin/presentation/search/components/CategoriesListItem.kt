42d0e430ad76e6f6326ef94bfcfa619fabb4367e
Mubashir P A <mubashirpa2002@gmail.com>
2024-01-28 12:15:48 +0530

    Added CategoriesListItem

---
+++ composeApp/src/commonMain/kotlin/presentation/search/components/CategoriesListItem.kt
@@ -0,0 +1,39 @@
+package presentation.search.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+
+@Composable
+fun CategoriesListItem(
+    name: String,
+    imageUrl: String,
+    modifier: Modifier = Modifier,
+) {
+    Card(modifier = modifier) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            KamelImage(
+                resource = asyncPainterResource(imageUrl),
+                contentDescription = null,
+                modifier = Modifier.size(56.dp),
+            )
+            Text(
+                text = name,
+                modifier = Modifier.padding(horizontal = 8.dp),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.labelLarge,
+            )
+        }
+    }
+}
