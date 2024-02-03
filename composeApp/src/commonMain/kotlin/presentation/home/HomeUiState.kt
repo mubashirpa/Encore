0750c832180a748280f4c5a19b417014a1d03461
Mubashir P A <mubashirpa2002@gmail.com>
2024-02-03 21:24:05 +0530

    Renamed data classes.

--- composeApp/src/commonMain/kotlin/presentation/home/HomeUiState.kt
+++ composeApp/src/commonMain/kotlin/presentation/home/HomeUiState.kt
@@ -3,11 +3,11 @@ package presentation.home
 import core.Result
 import domain.model.saavn.LaunchData
 import domain.model.spotify.playlists.PlaylistsItem
-import domain.model.spotify.users.topItems.UsersTrackItem
+import domain.model.spotify.users.topItems.UsersTopTrackItem
 
 data class HomeUiState(
     val accessToken: String = "",
     val featuredPlaylistsResult: Result<List<PlaylistsItem>> = Result.Empty(),
     val launchDataResult: Result<LaunchData> = Result.Empty(),
-    val usersTrackItemResult: Result<List<UsersTrackItem>> = Result.Empty(),
+    val usersTrackItemResult: Result<List<UsersTopTrackItem>> = Result.Empty(),
 )
