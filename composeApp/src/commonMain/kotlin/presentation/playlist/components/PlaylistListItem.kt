843d1b2580ba6276136d73a3c38089fc8b792a5e
Mubashir P A <mubashirpa2002@gmail.com>
2024-02-05 13:21:07 +0530

    Added PlaylistListItem.

---
+++ composeApp/src/commonMain/kotlin/presentation/playlist/components/PlaylistListItem.kt
@@ -0,0 +1,68 @@
+package presentation.playlist.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+
+@Composable
+fun PlaylistListItem(
+    name: String,
+    imageUrl: String,
+    artists: String,
+    onClick: () -> Unit,
+) {
+    ListItem(
+        headlineContent = {
+            Text(
+                text = name,
+                fontWeight = FontWeight.Bold,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+            )
+        },
+        modifier = Modifier.clickable(onClick = onClick),
+        supportingContent = {
+            Text(
+                text = artists,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+            )
+        },
+        leadingContent = {
+            KamelImage(
+                resource = asyncPainterResource(imageUrl),
+                contentDescription = null,
+                modifier =
+                    Modifier
+                        .size(56.dp)
+                        .clip(MaterialTheme.shapes.medium),
+                contentScale = ContentScale.Crop,
+            )
+        },
+        trailingContent = {
+            IconButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = null,
+                )
+            }
+        },
+    )
+}
