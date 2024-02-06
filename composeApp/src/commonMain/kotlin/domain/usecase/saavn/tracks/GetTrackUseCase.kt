9b957f6a97fcec9715aced4f48c59da4fc46c349
Mubashir P A <mubashirpa2002@gmail.com>
2024-02-06 18:05:06 +0530

    Fixed return type.

--- composeApp/src/commonMain/kotlin/domain/usecase/saavn/tracks/GetTrackUseCase.kt
+++ composeApp/src/commonMain/kotlin/domain/usecase/saavn/tracks/GetTrackUseCase.kt
@@ -8,11 +8,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class GetTrackUseCase(private val saavnRepository: SaavnRepository) {
-    operator fun invoke(id: String): Flow<Result<List<SongItem>>> =
+    operator fun invoke(id: String): Flow<Result<SongItem>> =
         flow {
             try {
                 emit(Result.Loading())
-                val track = saavnRepository.getTrack(id).songs?.map { it.toSongItem() }
+                val track = saavnRepository.getTrack(id).songs?.firstOrNull()?.toSongItem()
                 emit(Result.Success(track))
             } catch (e: Exception) {
                 emit(Result.Error(message = e.message.toString()))
